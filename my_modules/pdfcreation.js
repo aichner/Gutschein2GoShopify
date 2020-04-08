a68b0918cef07e9e328fd22283044e80396352e6
Klompara <36262189+Klompara@users.noreply.github.com>
2020-04-09 00:35:56 +0200

    pdf creation concept

--- my_modules/pdfcreation.js
+++ my_modules/pdfcreation.js
@@ -1,24 +1,32 @@
 const PDFDocument = require('pdfkit');
 const shopify = require('./shopify');
 
-function getCoupon (req, res) {
-    let doc = new PDFDocument()
-    let name = req.body.name;
-    let anschrift = req.body.anschrift;
-    let ausstelldatum = req.body.ausstelldatum;
+function getCoupon(req, res) {
+    let data = req.body.coupons;
+    let doc = new PDFDocument();
+    data.forEach((coupon) => {
+        drawSingleCoupon(doc, coupon);
+    });
+
 
     // Setting response to 'attachment' (download).
     // If you use 'inline' here it will automatically open the PDF
     res.setHeader('Content-disposition', 'attachment; filename="gutschein.pdf"');
     res.setHeader('Content-type', 'application/pdf');
 
-    doc.y = 300;
-    doc.text(name, 50, 50);
-    doc.text(anschrift, 50, 150);
-    doc.text(ausstelldatum, 50, 250);
     doc.pipe(res);
     doc.end();
 };
+
+function drawSingleCoupon(doc, coupon, position) {
+    yOffset = position * doc.page.height / 3;
+
+    doc.rect(0, 0, doc.page.width, yOffset + doc.page.height / 3).fill(coupon.primaryColor);
+    doc.fill(coupon.secondaryColor).stroke();
+    doc.fontSize(16);
+    doc.text(coupon.text, 10, yOffset + 10, { lineBreak: false });
+    
+}
 
 
 module.exports.getCoupon = getCoupon;
