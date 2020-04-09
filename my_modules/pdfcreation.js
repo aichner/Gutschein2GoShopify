af4dcba0e0bbf7f7124d9c580b09f992a579fa14
Klompara <36262189+Klompara@users.noreply.github.com>
2020-04-09 20:06:06 +0200

    document creation with front+backside and qr-code implementation, no design

--- my_modules/pdfcreation.js
+++ my_modules/pdfcreation.js
@@ -1,31 +1,67 @@
 const PDFDocument = require('pdfkit');
-const shopify = require('./shopify');
+const request = require('request');
 
-function getCoupon(req, res) {
+async function getCoupon(req, res) {
     let data = req.body.coupons;
     let doc = new PDFDocument();
-    data.forEach((coupon) => {
-        drawSingleCoupon(doc, coupon);
-    });
-
+    let tempCoupons = [];
+    for (let i = 0; i < data.length; i++) {
+        if (i % 3 == 0 && i != 0) {
+            doc.addPage();
+            for (let j = 0; j < tempCoupons.length; j++) {
+                await drawSingleCouponBack(doc, tempCoupons[j], j);
+            }
+            doc.addPage();
+            tempCoupons = []; // clear array to save new 3 coupons
+        }
+        drawSingleCouponFront(doc, data[i], i % 3);
+        tempCoupons.push(data[i]); // store the 3 coupons for the backside
+    }
+    if (tempCoupons.length != 0) {
+        doc.addPage();
+        for (let j = 0; j < tempCoupons.length; j++) {
+            await drawSingleCouponBack(doc, tempCoupons[j], j);
+        }
+    }
 
     // Setting response to 'attachment' (download).
     // If you use 'inline' here it will automatically open the PDF
     res.setHeader('Content-disposition', 'attachment; filename="gutschein.pdf"');
     res.setHeader('Content-type', 'application/pdf');
-
     doc.pipe(res);
     doc.end();
 };
 
-function drawSingleCoupon(doc, coupon, position) {
+function drawSingleCouponFront(doc, coupon, position) {
     yOffset = position * doc.page.height / 3;
-
-    doc.rect(0, 0, doc.page.width, yOffset + doc.page.height / 3).fill(coupon.primaryColor);
+    doc.rect(0, yOffset + 0, doc.page.width, doc.page.height / 3).fill(coupon.primaryColor);
     doc.fill(coupon.secondaryColor).stroke();
     doc.fontSize(16);
     doc.text(coupon.text, 10, yOffset + 10, { lineBreak: false });
-    
+}
+
+async function drawSingleCouponBack(doc, coupon, position) {
+    yOffset = position * doc.page.height / 3;
+    doc.rect(0, yOffset + 0, doc.page.width, doc.page.height / 3).fill(coupon.primaryColor);
+    doc.fill(coupon.secondaryColor).stroke();
+    doc.fontSize(20);
+    doc.text('This is the backside of the coupon with order date: ' + coupon.orderDate, 10, yOffset + 20, { lineBreak: false });
+    await addQrCode(doc, 'tokenblablalba', doc.page.width - 150, yOffset + doc.page.height / 3 - 150);
+}
+
+async function addQrCode(doc, token, x, y) {
+    return new Promise((resolve, reject) => {
+        request({ url: 'http://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=L&chl=https://g2g.at/verify?token=' + token, encoding: null }, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                var img = new Buffer(body, 'base64');
+                doc.image(img, x, y);
+                resolve();
+            } else {
+                reject();
+                console.error(error + '\n status code: ' + response.statusCode);
+            }
+        });
+    });
 }
 
 
